d426b7cea964edc3718d33d56bb00931f29de1b4
John A. Debay <john@highorderbit.com>
2009-06-23 23:05:04 -0600

    Fixed small bug with active account management.

--- src/iphone/Classes/app-controller/TwitchAppDelegate.m
+++ src/iphone/Classes/app-controller/TwitchAppDelegate.m
@@ -289,7 +289,8 @@
         TwitterCredentials * activeAccount =
             [accountsDisplayMgr selectedAccount];
 
-        if (activeAccount != self.activeCredentials.credentials) {
+        if (activeAccount &&
+            activeAccount != self.activeCredentials.credentials) {
             NSLog(@"Switching account to: '%@'.", activeAccount);
             [self broadcastActivatedCredentialsChanged:activeAccount];
         }
