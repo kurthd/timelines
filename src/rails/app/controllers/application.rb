490687527b6b208e7413c69b41625a626a2061a2
John A. Debay <john@highorderbit.com>
2009-06-26 22:19:47 -0600

    Force redirect to SSL.

--- src/rails/app/controllers/application.rb
+++ src/rails/app/controllers/application.rb
@@ -4,6 +4,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
+  before_filter :redirect_to_ssl
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '6b754e6c2b5803ea0193ecaa9445d38a'
@@ -12,4 +14,8 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+  def redirect_to_ssl
+    redirect_to :protocol => "https://" unless (request.ssl? or local_request?)
+  end
 end
