a2cc78642c58d326b13334d5e4ce7961c9dfc2d2
John A. Debay <john@highorderbit.com>
2009-07-07 15:38:24 -0600

    Added functional tests for the device registration code.

--- src/rails/app/controllers/device_controller.rb
+++ src/rails/app/controllers/device_controller.rb
@@ -1,13 +1,9 @@
 class DeviceController < ApplicationController
-  @@encryption_secret_file = File.join(RAILS_ROOT, 'config', 'garbage')
-
   protect_from_forgery :except => [ :register ]
 
   def register
-    logger.debug "Registration request received."
-
     if request.post?
-      logger.debug "Received post request: #{request}."
+      logger.info "Received post request."
       logger.debug "device-token: '#{params[:devicetoken]}.'"
 
       token = params[:devicetoken]
@@ -17,10 +13,23 @@ class DeviceController < ApplicationController
       keys = results[1]
       secrets = results[2]
 
-      iphone = Iphone.find_or_create(token)
-      self.register_accounts(iphone, usernames, keys, secrets)
-    else
-      @devices = Iphone.find(:all)
+      if usernames.length != keys.length || usernames.length != secrets.length
+        # raising an exception seems to break tests (??), so just
+        # checking manually for now
+        logger.error "Received #{usernames.length} usernames, " +
+          "#{keys.length} keys, and #{secrets.length} secrets. Usernames, " +
+          "keys, and secrets must match."
+      else
+        iphone = Iphone.find_or_create(token)
+        @subscriptions = DeviceSubscription.set_subscriptions_for_device(
+          iphone, usernames, keys, secrets)
+
+        logger.info "iPhone #{iphone} has #{@subscriptions.length} " +
+          "subscription(s) after registration:"
+        @subscriptions.each do |s|
+          logger.info "\t#{s.twitter_user}"
+        end
+      end
     end
   end
 
@@ -41,107 +50,6 @@ class DeviceController < ApplicationController
       end
     end
 
-    if (usernames.length != keys.length || usernames.length != secrets.length)
-      raise Exception.new("Received #{usernames.length} usernames, " +
-        "#{keys.length} keys, and #{secrets.length} for device token: " +
-        "#{token}. Usernames, keys, and secrets must match. Aborting " +
-        "registration.")
-    end
-
     [ usernames, keys, secrets ]
-  end
-
-  def delete_subscription(subscription)
-    user = TwitterUser.find_by_id(subscription.twitter_user_id)
-    if user != nil
-      user.delete
-    else
-      logger.warn "Failed to find user with ID: #{subscription.twitter_user_id}"
-        "for subscription: #{subscription} but one should exist."
-      users = TwitterUser.find(:all)
-      users.each { |user| logger.warn "user: #{user}" }
-    end
-    subscription.delete
-  end
-
-  def remove_stale_subscriptions(iphone, accounts)
-    deleted_subscriptions = Array.new
-
-    # remove any subscription not contained within the array
-    old_subscriptions = DeviceSubscription.find(:all,
-      :conditions => [ 'iphone_id = ?', iphone.id ])
-    new_users = accounts.keys
-
-    old_subscriptions.each do |subscription|
-      user = TwitterUser.find(subscription.twitter_user_id)
-
-      if (user != nil)
-        if !new_users.include?(user.username)
-          subscription.delete
-          deleted_subscriptions << subscription
-        end
-      end
-    end
-  end
-
-  def remove_stale_iphone(iphone)
-    subscription =
-      DeviceSubscription.find(:all,
-        :conditions => [ 'iphone_id = ?', iphone.id ])
-
-    if subscription == nil || subscription.length == 0
-      # this iphone is no longer subscribed for any notifications
-      iphone.delete
-    end
-  end
-
-  def remove_stale_twitter_users(deleted_subscriptions)
-    deleted_subscriptions.each do |subscription|
-      user = TwitterUser.find(subscription.twitter_user_id)
-      if (user != nil)
-        subscriptions = DeviceSubscription.find_all_by_twitter_user_id(user.id)
-        if (subscriptions != nil && subscriptions.length == 0)
-          user.delete
-        end
-      end
-    end
-  end
-
-  def register_account(iphone, username, key, secret)
-    logger.info "Creating new account for user #{username} => #{iphone}."
-    twitter_user =
-      self.create_twitter_user(username, key, secret)
-    subscription = self.create_subscription(twitter_user.id, iphone.id)
-
-    begin
-      # connects to twitter; save the user's last dm and mention
-      suscriber_status =
-        SubscriberStatus.initialize_from_twitter(twitter_user, subscription)
-    rescue
-      logger.warn "Failed to check current status for user: #{twitter_user}: " +
-        "#{$!}"
-      # just save zeros; it'll get filled in the next time the polling job runs
-      SubscriberStatus.create(
-        :last_direct_message => '0',
-        :last_mention => '0',
-        :device_subscription_id => subscription.id)
-    end
-
-    subscription
-  end
-
-  def create_twitter_user(username, key, secret)
-    twitter_user = TwitterUser.new(:username => username,
-                                   :key => key,
-                                   :secret => secret)
-    twitter_user.save!
-    twitter_user
-  end
-
-  def create_subscription(twitter_user_id, iphone_id)
-    subscription = DeviceSubscription.new(:twitter_user_id => twitter_user_id,
-                                          :iphone_id => iphone_id)
-    subscription.save!
-    subscription
   end
 end
--- src/rails/test/functional/device_controller_test.rb
+++ src/rails/test/functional/device_controller_test.rb
@@ -1,8 +1,91 @@
 require 'test_helper'
 
-class RegisterDeviceControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class DeviceControllerTest < ActionController::TestCase
+  def setup
+    @iphone = iphones(:johns_iphone)
+    @usernames = Array.new
+    @keys = Array.new
+    @secrets = Array.new
+
+    @usernames << twitter_users(:debay).username
+    @usernames << twitter_users(:highorderbit).username
+    @usernames << twitter_users(:kurthd).username
+
+    @keys << twitter_users(:debay).key
+    @keys << twitter_users(:highorderbit).key
+    @keys << twitter_users(:kurthd).key
+
+    @secrets << twitter_users(:debay).secret
+    @secrets << twitter_users(:highorderbit).secret
+    @secrets << twitter_users(:kurthd).secret
+  end
+
+  def teardown
+  end
+
+  # one iphone maps to a single account
+  test "single account registration" do
+    params = self.build_params(
+      @iphone, @usernames[0, 1], @secrets[0, 1], @keys[0, 1])
+
+    post(:register, params)
+    assert_response :success
+    assert_not_nil assigns(:subscriptions)
+    assert_equal 1, assigns(:subscriptions).length
+  end
+
+  test "multiple account registration" do
+    params = self.build_params(@iphone, @usernames, @secrets, @keys)
+
+    post(:register, params)
+    assert_response :success
+    assert_not_nil assigns(:subscriptions)
+    assert_equal @usernames.length, assigns(:subscriptions).length
+  end
+
+  test "no account registration" do
+    params = self.build_params(@iphone, [], [], [])
+
+    post(:register, params)
+    assert_response :success
+    assert_not_nil assigns(:subscriptions)
+    assert_equal 0, assigns(:subscriptions).length
+  end
+
+  test "invalid registration" do
+    params = self.build_params(@iphone, @usernames[0, 1], @keys[0, 1], [])
+
+    post(:register, params)
+    assert_response :success
+    assert_nil assigns(:subscriptions)
+  end
+
+  test "get request" do
+    get :register, self.build_params(@iphone, @usernames, @keys, @secrets)
+    assert_nil assigns(:subscriptions)
+    assert_response :success
+  end
+
+  @private
+
+  def build_params(iphone, usernames, keys, secrets)
+    params = Hash.new
+    params['devicetoken'] = iphone.device_token
+
+    usernames.each_index do |i|
+      if i < usernames.length
+        params["username#{i}"] = "#{usernames[i]}"
+      end
+
+      if i < keys.length
+        params["key#{i}"] = "#{keys[i]}"
+      end
+
+      if i < secrets.length
+        params["secret#{i}"] = "#{secrets[i]}"
+      end
+    end
+
+    params
   end
 end
