d1ade31c4b1eece4ab4733b0e9a52a8003001f05
Doug Kurth <doug@highorderbit.com>
2009-07-23 13:55:48 -0600

    Fixed default avatar retain counts in various views' headers

--- src/Classes/timeline/TweetDetailsViewController.m
+++ src/Classes/timeline/TweetDetailsViewController.m
@@ -442,7 +442,7 @@ static UIImage * defaultAvatar;
 + (UIImage *)defaultAvatar
 {
     if (!defaultAvatar)
-        defaultAvatar = [UIImage imageNamed:@"DefaultAvatar.png"];
+        defaultAvatar = [[UIImage imageNamed:@"DefaultAvatar.png"] retain];
 
     return defaultAvatar;
 }
