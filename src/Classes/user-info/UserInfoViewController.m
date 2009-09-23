e4d6474a22be935eceac4367dc70aeb75cb8d81a
Doug Kurth <doug@highorderbit.com>
2009-09-23 16:25:24 -0600

    Fixed bug loading default avatar image in user search view

--- src/Classes/user-info/UserInfoViewController.m
+++ src/Classes/user-info/UserInfoViewController.m
@@ -587,7 +587,7 @@ static UIImage * defaultAvatar;
 + (UIImage *)defaultAvatar
 {
     if (!defaultAvatar)
-        defaultAvatar = [[UIImage imageNamed:@"DefaultAvatar .png"] retain];
+        defaultAvatar = [[UIImage imageNamed:@"DefaultAvatar.png"] retain];
 
     return defaultAvatar;
 }
